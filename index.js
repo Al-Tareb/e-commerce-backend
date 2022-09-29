5c5109af8550339bbc8a04270fb8a7a02dd074b9
Al-Tareb <altareb_a1a1@yahoo.com>
2022-09-29 16:35:11 +0200

    change origin

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use("/order", orderRouter);
 app.use("/api/stripe", stripeRouter)
 
 // app.use(function(req, res, next){
-//     res.header("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // })
